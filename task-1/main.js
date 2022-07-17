a6bbc83d24365c90c4cbd9f3bd2193f03b36b0d5
nesrinmirza <106273121+nesrinmirza@users.noreply.github.com>
2022-07-17 11:33:11 -0700

    Update main.js

--- task-1/main.js
+++ task-1/main.js
@@ -1,9 +1,8 @@
-const prompt = require('prompt');
-prompt.start();
-
-
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+function disk(n) {
+   let a = parseInt(n/100)
+   let b = parseInt(n/10)%10
+   let c =n%10
+   
+    console.log(a*100 + (b/2)*30 + c*2)
+}
+disk(345)
