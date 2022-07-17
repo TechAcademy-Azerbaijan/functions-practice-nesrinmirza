0cee8889c2839d9caff611ef77c8d97d95f18ccb
nesrinmirza <106273121+nesrinmirza@users.noreply.github.com>
2022-07-17 11:47:59 -0700

    Update main.js

--- task-2/main.js
+++ task-2/main.js
@@ -1,9 +1,15 @@
-const prompt = require('prompt');
-prompt.start();
-
-
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+function sade (n) {
+    count = 0;
+    for (let i = 2; i <= n; i++) {
+        if (n%i==0) {
+            count++;
+        }
+    }
+    if (count>1) {
+        console.log("NO");
+    }
+    else if (count==1) {
+      console.log("YES");  
+    }
+}
+sade(18)
